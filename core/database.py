f20dd189e31581e1adc493d48efb6b8245bec655
igormago <=>
2023-09-05 09:08:56 -0300

    Iniciando repositorio

---
+++ core/database.py
@@ -0,0 +1,32 @@
+from conf.configparser import conf
+from pymongo import MongoClient
+
+class Database:
+    """Interact with conf variables."""
+
+    print(conf['mongo']['host'])
+    sm_session = MongoClient(host=conf['mongo']['host'],
+                             port=int(conf['mongo']['port'])).get_database('sportmonks')
+
+    bf_session = MongoClient(host=conf['mongo']['host'],
+                             port=int(conf['mongo']['port'])).get_database('betfair')
+
+    be_session = MongoClient(host=conf['mongo']['host'],
+                             port=int(conf['mongo']['port'])).get_database('betexplorer')
+
+    @classmethod
+    def get_session_sportmonks(cls):
+        return cls.sm_session
+
+    @classmethod
+    def get_session_betfair(cls):
+        return cls.bf_session
+
+    @classmethod
+    def get_session_betexplorer(cls):
+        return cls.be_session
+
+    @classmethod
+    def open_new_session_betexplorer(cls):
+        return MongoClient(host=conf['mongo']['host'],
+                           port=int(conf['mongo']['port'])).get_database('betexplorer')
